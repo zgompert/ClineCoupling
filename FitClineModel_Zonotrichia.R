1d86bc41c00138794476fe98ac05dafdb8526870
Zach Gompert <31893662+zgompert@users.noreply.github.com>
2023-01-05 16:26:40 -0700

    Add files via upload

---
+++ FitClineModel_Zonotrichia.R
@@ -0,0 +1,144 @@
+## script to format the Zonotrichia data and fit the cline model
+## load libraries
+library(data.table)
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+library(scales) ## for plotting
+
+## read main data file, 
+gdat<-fread("Zonotrichia_SNPs_6419loci_169ind.csv",header=TRUE,sep=",")
+N<-dim(gdat)[1];L<-dim(gdat)[2]-2
+
+gdat<-as.data.frame(gdat)
+Gint<-matrix(NA,nrow=N,ncol=L)
+for(i in 1:L){
+	k<-i+2
+	dd<-unlist(gdat[,k])
+	dd[dd=="N"]<-NA
+	aa<-sort(unique(dd)[unique(dd) %in% c("A","C","G","T")])
+	Gint[dd==aa[1],i]<-0
+	Gint[dd==aa[2],i]<-2
+	het<-unique(dd)[!unique(dd) %in% c("A","C","G","T")]
+	het<-het[which(is.na(het)==FALSE)]
+	Gint[dd==het,i]<-1
+}
+
+
+## get ids from same file
+ids<-gdat[,c(1,2)]
+
+## simple parental allele frequencies from genotypes
+P1<-apply(Gint[ids[,2]=="P1",],2,mean,na.rm=TRUE)/2
+P2<-apply(Gint[ids[,2]=="P2",],2,mean,na.rm=TRUE)/2
+
+## ancestry informative
+dp<-abs(P1-P2)
+sum(dp > .3,na.rm=TRUE) ## 97
+miss<-apply(is.na(Gint)==TRUE,2,mean)
+# max missing ~20%
+
+anc<-which(dp > .3)
+plot(P1[anc],P2[anc])
+
+hyb<-which(ids[,2]=="H")
+length(hyb)## 22 hybrids
+
+## genotype matrix for ancestry informative SNPs for hybrids
+GhybI<-Gint[hyb,anc]
+Miss<-is.na(GhybI)+0
+GhybIM<-GhybI
+GhybIM[Miss==1]<-1
+
+############# estiamte hybrid indexes##############
+## number of loci and individuals, rows=ids
+L<-dim(GhybI)[2];N<-dim(GhybI)[1]
+## make data list with number of loci, number of inds, genotype matrix and parental allele freqs.
+dat<-list(L=L,N=N,G=GhybIM,P0=P1[anc],P1=P2[anc],miss=Miss)
+fit_hi<-stan("../missing_hindex.stan",data=dat)
+
+## extract posteriors for hybrid index
+hi<-extract(fit_hi,"H")
+## point esimte
+hi_est<-apply(hi[[1]],2,median)
+## nice broad distribution, lots around .5
+
+############ fit clines #####################
+## make data list
+dat<-list(L=L,N=N,G=GhybIM,H=hi_est,P0=P1[anc],P1=P2[anc],miss=Miss)
+## use 8 shorter chains to speed up the anlaysis
+n_chains<-8
+## helps to initialize cline parameters to reasonable values
+initf<-function(L=length(anc),chain_id=1){
+	list(center=runif(L,.3,.7),v=runif(L,.9,1.1),alpha=chain_id)
+}
+init_ll <- lapply(1:n_chains, function(id) initf(chain_id = id))
+
+## fit model
+fit<-stan("../missing_clinemod_nosz.stan",data=dat,chains=n_chains,iter=1500,warmup=1000,init=init_ll)
+
+save(list=ls(),file="clines_Zonotrichia.rdat")
+
+## extract MCMC output
+oo<-extract(fit)
+
+## focus on key SD parameters, variability in cline center and width
+
+## mixing looks good
+plot(oo$sv)
+plot(oo$sc)
+
+## here are the estiamtes we want, main output from the analysis
+## width, on logit scale
+quantile(oo$sv,probs=c(.5,.05,.95)) ## median and 90% ETPIs
+#      50%        5%       95% 
+#0.5670591 0.4902761 0.6598406 
+## center, on log scale
+quantile(oo$sc,probs=c(.5,.05,.95)) ## median and 90% ETPIs
+#      50%        5%       95% 
+#0.6302163 0.4987334 0.7888854
+## actual SDs, not model parameter and thus not dependent on soft centering
+quantile(oo$sdv,probs=c(.5,.05,.95))
+#      50%        5%       95%
+#0.4202282 0.3727058 0.4667764 
+quantile(oo$sdc,probs=c(.5,.05,.95))
+#      50%        5%       95% 
+#0.6256537 0.5064085 0.7559450
+
+## check soft-centering, not great, but still okay, this is an issue with low coupling I think
+mean(as.vector(log(oo$center/(1-oo$center))))
+#[1] -0.04865212
+mean(as.vector(log10(oo$v)))
+#[1]  0.3757447
+
+## just for fun (and maybe of later use) here are the SD on the raw parameter scale
+quantile(oo$sdrv,probs=c(.5,.05,.95))
+#     50%       5%      95% 
+#2.653592 2.336601 2.941363
+quantile(oo$sdrc,probs=c(.5,.05,.95))
+#      50%        5%       95% 
+#0.1439778 0.1196989 0.1688117
+
+## lets look at some clines
+## h = hybrid index, v = width, u = center
+## null is v = 1, u = 0
+llcline<-function(h=NA,v=0,u=0){
+        phi<-(h^v)/(h^v + (1-h)^v * exp(u))
+        return(phi)
+}
+
+hh<-seq(0,1,0.01)
+
+vest<-apply(oo$v,2,quantile,probs=c(.5,.05,.95))
+uest<-apply(oo$u,2,quantile,probs=c(.5,.05,.95))
+
+kk<-1:L
+
+plot(hh,hh,type='n',xlab="Hybrid index",ylab="Pr. P2  ancestry",cex.lab=1.3)
+for(i in kk){
+	y<-llcline(h=hh,v=vest[1,i],u=uest[1,i])
+	lines(hh,y,col=alpha("darkgray",.8))
+}
+abline(a=0,b=1,lty=2)
+
+save(list=ls(),file="clines_Zonotrichia.rdat")
