d288113cae3be951c1958ea28988f0808fe6526d
Zach Gompert <31893662+zgompert@users.noreply.github.com>
2023-01-05 15:49:26 -0700

    Add files via upload

---
+++ FitClineModel_Nematocharax.R
@@ -0,0 +1,151 @@
+## script to format the Nematocharax data and fit the cline model
+## load libraries
+library(data.table)
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+library(scales) ## for plotting
+
+## read main data file, structure format
+gdat<-read.table("genomic_unlinked_loci.str",header=FALSE)
+dim(gdat)
+#[1] 110 1269 
+
+## -9 = NA other numbers alleles, no more than 2 per locus
+gdat[gdat==-9]<-NA
+gdat1<-gdat[seq(1,110,2),-1]
+gdat2<-gdat[seq(2,110,2),-1]
+low<-as.numeric(apply(gdat[,-1],2,min,na.rm=TRUE))
+hi<-as.numeric(apply(gdat[,-1],2,max,na.rm=TRUE))
+Gint<-gdat1
+Gint[gdat1!=gdat2]<-1
+
+for(i in 1:dim(Gint)[2]){
+	Gint[which(gdat1[,i]==low[i] & gdat2[,i]==low[i]),i]<-0
+	Gint[which(gdat1[,i]==hi[i] & gdat2[,i]==hi[i]),i]<-2
+}
+
+Gint<-as.matrix(Gint)
+dim(Gint)
+#[1] 55 1268
+
+
+## read ids, ID and parent/hybird, order matches genotype
+ids<-read.table("Nematocharax_pop_desig.txt",header=FALSE)
+mean(unique(gdat[,1])==ids[,1])
+
+## simple parental allele frequencies from genotypes
+P1<-apply(Gint[ids[,2]=="P1",],2,mean,na.rm=TRUE)/2
+P2<-apply(Gint[ids[,2]=="P2",],2,mean,na.rm=TRUE)/2
+
+## ancestry informative
+dp<-abs(P1-P2)
+sum(dp > .3,na.rm=TRUE) ## 270 
+pctMiss<-apply(is.na(Gint),2,mean)
+sum(dp > .3 & pctMiss < .3,na.rm=TRUE) #52
+
+anc<-which(dp > .3 & pctMiss < .3)
+plot(P1[anc],P2[anc])
+
+hyb<-which(ids[,2]=="H")
+length(hyb)## 41 hybrids
+
+## genotype matrix for ancestry informative SNPs for hybrids
+GhybI<-Gint[hyb,anc]
+Miss<-is.na(GhybI)+0
+GhybIM<-GhybI
+GhybIM[Miss==1]<-1
+
+############# estiamte hybrid indexes##############
+## number of loci and individuals, rows=ids
+L<-dim(GhybI)[2];N<-dim(GhybI)[1]
+## make data list with number of loci, number of inds, genotype matrix and parental allele freqs.
+dat<-list(L=L,N=N,G=GhybIM,P0=P1[anc],P1=P2[anc],miss=Miss)
+fit_hi<-stan("../missing_hindex.stan",data=dat)
+
+## extract posteriors for hybrid index
+hi<-extract(fit_hi,"H")
+## point esimte
+hi_est<-apply(hi[[1]],2,median)
+## full distn of hybrids
+
+############ fit clines #####################
+## make data list
+dat<-list(L=L,N=N,G=GhybIM,H=hi_est,P0=P1[anc],P1=P2[anc],miss=Miss)
+## use 8 shorter chains to speed up the anlaysis
+n_chains<-8
+## helps to initialize cline parameters to reasonable values
+initf<-function(L=length(anc),chain_id=1){
+	list(center=runif(L,.3,.7),v=runif(L,.9,1.1),alpha=chain_id)
+}
+init_ll <- lapply(1:n_chains, function(id) initf(chain_id = id))
+
+## fit model
+fit<-stan("../missing_clinemod_nosz.stan",data=dat,chains=n_chains,iter=1500,warmup=1000,init=init_ll)
+
+save(list=ls(),file="clines_Nematocharax.rdat")
+
+## extract MCMC output
+oo<-extract(fit)
+
+## focus on key SD parameters, variability in cline center and width
+
+## mixing looks good
+plot(oo$sv)
+plot(oo$sc)
+
+## here are the estiamtes we want, main output from the analysis
+## width, on logit scale
+quantile(oo$sv,probs=c(.5,.05,.95)) ## median and 90% ETPIs
+#      50%        5%       95% 
+#0.4085494 0.3145556 0.5170722 
+## center, on log scale
+quantile(oo$sc,probs=c(.5,.05,.95)) ## median and 90% ETPIs
+#      50%        5%       95% 
+#0.7967852 0.6357023 1.0105993 
+## actual SDs, not model parameter and thus not dependent on soft centering
+quantile(oo$sdv,probs=c(.5,.05,.95))
+#      50%        5%       95%
+#0.3422098 0.2775948 0.4052102 
+quantile(oo$sdc,probs=c(.5,.05,.95))
+#      50%        5%       95% 
+#0.7970405 0.6683287 0.9454682  
+
+
+## check soft-centering, okay
+mean(as.vector(log(oo$center/(1-oo$center))))
+#[1] 0.022904
+mean(as.vector(log10(oo$v)))
+#[1] 0.2167173
+
+## just for fun (and maybe of later use) here are the SD on the raw parameter scale
+quantile(oo$sdrv,probs=c(.5,.05,.95))
+#     50%       5%      95% 
+#1.854102 1.239098 2.407322  
+quantile(oo$sdrc,probs=c(.5,.05,.95))
+#      50%        5%       95% 
+#0.1764848 0.1534069 0.2006976 
+
+## lets look at some clines
+## h = hybrid index, v = width, u = center
+## null is v = 1, u = 0
+llcline<-function(h=NA,v=0,u=0){
+        phi<-(h^v)/(h^v + (1-h)^v * exp(u))
+        return(phi)
+}
+
+hh<-seq(0,1,0.01)
+
+vest<-apply(oo$v,2,quantile,probs=c(.5,.05,.95))
+uest<-apply(oo$u,2,quantile,probs=c(.5,.05,.95))
+
+
+plot(hh,hh,type='n',xlab="Hybrid index",ylab="Pr P2 ancestry",cex.lab=1.3)
+for(i in 1:L){
+	y<-llcline(h=hh,v=vest[1,i],u=uest[1,i])
+	lines(hh,y,col=alpha("darkgray",.8))
+}
+abline(a=0,b=1,lty=2)
+
+save(list=ls(),file="clines_Nematocharax.rdat")
+
